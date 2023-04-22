09b64d2096602156cb477f115febaf65b094b5f6
khadjaa <islomkhadja2001@icloud.com>
2023-04-22 17:43:26 +0300

    added ChooseScreen

--- my-app/src/components/ChooseScreen/ChooseScreen.tsx
+++ my-app/src/components/ChooseScreen/ChooseScreen.tsx
@@ -1,14 +1,33 @@
 import React from 'react';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {AppStoreType} from "../../store/store";
+import {chooseOrderTypeAC} from "../../store/orderInfoReducer";
 
 const ChooseScreen = () => {
 
-    const orderType = useSelector((state:AppStoreType) => state.info.order)
+    const orderType = useSelector((state: AppStoreType) => state.info.order)
+    const dispatch = useDispatch()
+
+    const chooseOrderTypeHandler = (orderType: string) => {
+        dispatch(chooseOrderTypeAC(orderType))
+    }
 
     return (
         <div>
-            {orderType.orderType}
+            <div className="card">
+                <h2>В зале</h2>
+                <p>Тренируйтесь дома или на улице в любое время, удобное для вас!</p>
+                <button onClick={() => chooseOrderTypeHandler('В ЗАЛЕ')}>Выбрать</button>
+            </div>
+
+            <div className="card">
+                <h2>С собой</h2>
+                <p>Посетите наш современный тренажерный зал и получите квалифицированную помощь тренера!</p>
+                <button onClick={() => chooseOrderTypeHandler('С СОБОЙ')}>Выбрать</button>
+            </div>
+            <h1>
+                {orderType.orderType}
+            </h1>
         </div>
     );
 };
