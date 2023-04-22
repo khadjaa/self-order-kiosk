b3dfa4e726b6a2b612faedfeae991c9ff032d5f3
khadjaa <islomkhadja2001@icloud.com>
2023-04-22 17:05:44 +0300

    added HomeScreen

---
+++ my-app/src/components/ChooseScreen/ChooseScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChooseScreen = () => {
+    return (
+        <div>
+            choose
+        </div>
+    );
+};
+
+export default ChooseScreen;
---
+++ my-app/src/components/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import s from './HomeScreen.module.css'
+import logo from '../../images/Logo_IV.png'
+import {useNavigate} from "react-router-dom";
+
+export default function HomeScreen() {
+    // const styles = useStyles();
+    const navigate = useNavigate()
+
+    return (
+        <div className={s.homeScreen}>
+            <div className={s.header}>
+                <img src={logo} alt="logo"/>
+            </div>
+            <div className={s.main}>
+                <button
+                    onClick={() => navigate('/choose')}
+                    className={s.buttonOrder}>
+                    Нажмите чтобы заказать
+                </button>
+            </div>
+        </div>
+
+
+    );
+}
+
