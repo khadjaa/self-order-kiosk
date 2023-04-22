b8915d020055625e6b3219d5b78c722c0538b7ad
khadjaa <islomkhadja2001@icloud.com>
2023-04-22 22:54:14 +0300

    deleted styles and added pictures

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import HomeScreen from "./components/HomeScreen/HomeScreen";
 import {Route, Routes} from "react-router-dom";
 import ChooseScreen from "./components/ChooseScreen/ChooseScreen";
 
 function App() {
   return (
-   <Routes>
-       <Route path={'/'} element={<HomeScreen/>}></Route>
-       <Route path={'/choose'} element={<ChooseScreen/>}></Route>
-   </Routes>
+      <div className={'App'}>
+          <Routes>
+              <Route path={'/'} element={<HomeScreen/>}></Route>
+              <Route path={'/choose'} element={<ChooseScreen/>}></Route>
+          </Routes>
+      </div>
   );
 }
 
