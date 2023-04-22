b5ebff761a9a2a2ce03527e3f3d7ecd3a27e97f3
khadjaa <islomkhadja2001@icloud.com>
2023-04-22 22:59:48 +0300

    added ChooseScreen

--- my-app/src/components/ChooseScreen/ChooseScreen.tsx
+++ my-app/src/components/ChooseScreen/ChooseScreen.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {AppStoreType} from "../../store/store";
+import {useDispatch} from "react-redux";
 import {chooseOrderTypeAC} from "../../store/orderInfoReducer";
+import logo from "../../images/Logo_IV.png";
+import logoSpb from '../../images/logo_sbp.png'
+import avaSoboy from '../../images/soboy.png'
+import vzale from '../../images/Vzale.png'
+import h from '../HomeScreen/HomeScreen.module.css'
+import s from './ChooseScreen.module.css'
 
 const ChooseScreen = () => {
 
-    const orderType = useSelector((state: AppStoreType) => state.info.order)
+    // const orderType = useSelector((state: AppStoreType) => state.info.order)
     const dispatch = useDispatch()
 
     const chooseOrderTypeHandler = (orderType: string) => {
@@ -13,21 +18,29 @@ const ChooseScreen = () => {
     }
 
     return (
-        <div>
-            <div className="card">
-                <h2>В зале</h2>
-                <p>Тренируйтесь дома или на улице в любое время, удобное для вас!</p>
-                <button onClick={() => chooseOrderTypeHandler('В ЗАЛЕ')}>Выбрать</button>
+        <div className={s.choose}>
+            <div className={h.header}>
+                <img src={logo} alt="logotype"/>
             </div>
-
-            <div className="card">
-                <h2>С собой</h2>
-                <p>Посетите наш современный тренажерный зал и получите квалифицированную помощь тренера!</p>
-                <button onClick={() => chooseOrderTypeHandler('С СОБОЙ')}>Выбрать</button>
+            <div className={s.cards}>
+                <div
+                    onClick={() => chooseOrderTypeHandler('В ЗАЛЕ')}
+                    className={s.card}>
+                    <img src={vzale} alt="vzale"/>
+                    <h1>В ЗАЛЕ</h1>
+                </div>
+                <div
+                    onClick={() => chooseOrderTypeHandler('С СОБОЙ')}
+                    className={s.card}>
+                    <img src={avaSoboy} alt="soboy"/>
+                    <h1>С СОБОЙ</h1>
+                </div>
             </div>
-            <h1>
-                {orderType.orderType}
-            </h1>
+            <div className={s.qr}>
+                <span><b>ВНИМАНИЕ! </b>
+                Оплата заказа по <b>QR</b> коду </span>
+                <img src={logoSpb} alt="logosbp"/>
+            </div>
         </div>
     );
 };
