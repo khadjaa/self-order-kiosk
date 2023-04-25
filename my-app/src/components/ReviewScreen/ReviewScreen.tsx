c45cc2a3fc433d985a0b710cffc7b19de5aae41c
khadjaa <islomkhadja2001@icloud.com>
2023-04-25 14:55:52 +0300

    added styles

--- my-app/src/components/ReviewScreen/ReviewScreen.tsx
+++ my-app/src/components/ReviewScreen/ReviewScreen.tsx
@@ -3,25 +3,34 @@ import {shallowEqual, useSelector} from "react-redux";
 import {AppStoreType} from "../../store/store";
 import {ProductsType} from "../../store/orderInfoReducer";
 import s from "../OrderScreen/OrderScreen.module.css";
+import r from './ReviewScreen.module.css'
 
 export const ReviewScreen = () => {
 
     const orderItems = useSelector<AppStoreType, ProductsType[]>(state => state.info.order.orderItems, shallowEqual)
-    const orderType = useSelector((state: AppStoreType) => state.info.order.orderType)
+
+    let sum = 0
 
     return (
-        <div className={s.review}>
+        <div className={r.review}>
             <p>ВАШ ЗАКАЗ</p>
             <div>
                 {orderItems.map(el => {
+                    sum += el.price
                     return (
-                        <div>
-                            {el.name}
+                        <div key={el.id}
+                             className={r.reviewCard}
+                        >
+                            <h1>{el.name}</h1>
+                            <p>{el.price} руб.</p>
                         </div>
                     )
                 })}
             </div>
-            <div className={s.orderButtons}>
+            <div>
+                <p>Total: {sum} руб.</p>
+            </div>
+            <div className={r.reviewButtons}>
                 <button className={s.cancelButton} onClick={() => {}}>Cancel</button>
                 <button className={s.payButton} onClick={() => {}}>Payment</button>
             </div>
