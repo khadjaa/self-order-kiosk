42f006592827baae0a87c7733e8978aa0fdea7a9
khadjaa <islomkhadja2001@icloud.com>
2023-05-06 20:06:34 +0300

    added compounds in OrderList

--- my-app/src/components/OrderList/OrderList.tsx
+++ my-app/src/components/OrderList/OrderList.tsx
@@ -9,15 +9,20 @@ export const OrderList = () => {
     const orderList = useSelector<AppStoreType, any>(state => state.info.order.orderList)
     console.log(orderList)
     const dispatch = useDispatch()
-
+    //orderList[0][0].compound[0].isDone
     return (
         <ul>
             {orderList.map((subArray: any, index: any) => (
                 <li key={index} className={s.borderLi}>
                     <p>#2134</p>
                     {subArray.map((item: any) => (
-                        <div key={item.id} >
+                        <div key={item.id} className={s.borderProd}>
                             <p>{item.name}</p>
+                            {
+                                item.compound.map((el: any) => el.isDone === false
+                                    ? <h6>Без: {el.name}</h6>
+                                    : null)
+                            }
                         </div>
                     ))}
                     <button onClick={() => dispatch(RemoveOrderListAC(index))}>DELETE</button>
